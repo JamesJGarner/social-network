1deda73dda9689944d77aee3848b2bd28d1a9330
James Garner <jamesjohngarner@gmail.com>
2016-03-11 10:30:51 +0000

    fixed url indentation

--- social_network/apps/posts/urls.py
+++ social_network/apps/posts/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from .views import ListPost, CreatePost, DetailPost, DeletePost
 
 urlpatterns = [
-	url(r'^$', ListPost.as_view(), name="list_post"),
+    url(r'^$', ListPost.as_view(), name="list_post"),
     url(r'^post/$', CreatePost.as_view(), name="create_post"),
     url(r'^posts/(?P<pk>\d+)/$', DetailPost.as_view(), name="detail_post"),
     url(r'^posts/(?P<pk>\d+)/delete/$', DeletePost.as_view(), name='delete_post'),
